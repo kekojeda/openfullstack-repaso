3290cea7b299544e51d48cef16cc7fd234b1b39b
Sergio Ojeda <keko@MacBook-Pro-de-Sergio.local>
2024-09-19 07:25:16 -0300

    countries 2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -3,6 +3,7 @@ import countriesService from "./services/countriesService";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [searchedCountries, setSearchedCountries] = useState([])
   const [searchCountriesValue, setSearchCountriesValue] = useState('');
 
   useEffect(() => {
@@ -15,6 +16,8 @@ function App() {
   const handleFilter = (event) => {
       console.log(event.target.value);
       setSearchCountriesValue(event.target.value)
+      setSearchedCountries(countries.filter((country) => country.name.common.toLowerCase().includes(searchCountriesValue.toLowerCase())))
+      
   }
 
   return (
@@ -24,15 +27,20 @@ function App() {
       <p>
         find countries <input onChange={handleFilter}/>
       </p>
-
       {
-
-        countries.map((country) => (
+        searchedCountries.length > 10 
+        ?
+        <p>Hay demasiados resultados, intenten una busqueda mas espesifica</p>
+        :
+        searchedCountries.map((country) => (
+          
           <li key={country.name.common}>
             {country.name.common}
           </li>
         ))
       }
+
+   
 
 
     </>
